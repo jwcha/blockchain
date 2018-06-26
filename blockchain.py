02cd9345229b356c0ce29b0b1a91323a149414ef
jwcha <neofanatic@gmail.com>
2018-06-25 18:12:27 -0700

    completed the mining endpoint!

--- blockchain.py
+++ blockchain.py
@@ -140,9 +140,39 @@ class Blockchain(object):
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
 
+    """
+    The Mining Endpoint
+    -------------------
+    This is where the 'magic' happens. The 'magic' can be broken down into
+    three things:
+        1. Calculate the Proof of Work.
+        2. Reward the miner(us) by adding a transaction granting us one coin.
+        3. Forge the new Block by adding it to the chain.
+    """
     @app.route('/mine', methods=['GET'])
     def mine():
-        return "We'll mine a new Block."
+        # We run the Proof of Work algorithm to get the next proof.
+        last_block = blockchain.last_block
+        last_proof = last_block['proof']
+        proof = blockchain.proof_of_work(last_proof)
+        # We must receive a reward for finding the proof.
+        # The sender is "0" to signify that this node has mined a new coin.
+        blockchain.new_transaction(
+                sender="0",
+                recipient=node_identifier,
+                amount=1,
+                )
+        # Forge the new Block by adding it to the chain.
+        previous_hash = blockchain.hash(last_block)
+        block = blockchain.new_block(proof, previous_hash)
+        response = {
+                'message': "New Block Forged.",
+                'index': block['index'],
+                'transactions': block['transactions'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash'],
+                }
+        return jsonify(response)
 
     @app.route('/transactions/new', methods=['POST'])
     def new_transaction():
@@ -165,7 +195,6 @@ class Blockchain(object):
                 'length': len(blockchain.chain),
                 }
         return jsonify(response), 200
-
 
     if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
